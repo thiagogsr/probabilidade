cfdcc81fc4ecebcf8c967461157b2e89ed26b1ed
Thiago Guimaraes <thiagogsr@gmail.com>
2014-06-09 09:33:29 -0300

    Deploy com rackup

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,10 @@ set :application, 'probabilidade'
 set :domain, '50.116.27.37'
 set :user, 'deploy'
 set :deploy_to, '/var/www/probabilidade'
+set :current_path, "#{deploy_to}/current"
 set :repository, 'git@github.com:thiagogsr/probabilidade.git'
 set :branch, 'master'
+set :pid, "#{deploy_to}/tmp/pids/#{application}.pid"
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
@@ -41,8 +43,8 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/shared/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+  queue! %[mkdir -p "#{deploy_to}/tmp/pids"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/tmp/pids"]
 end
 
 desc "Deploys the current version to the server."
@@ -53,51 +55,28 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'foreman:export'
 
     to :launch do
-      invoke :'foreman:restart'
+      invoke :'rackup:restart'
     end
   end
 end
 
-set_default :foreman_app,  lambda { application }
-set_default :foreman_user, lambda { user }
-set_default :foreman_log,  lambda { "#{deploy_to!}/#{shared_path}/log" }
-
-namespace :foreman do
-  desc 'Export the Procfile to Ubuntu upstart scripts'
-  task :export do
-    export_cmd = "rbenv sudo bundle exec foreman export upstart /etc/init -a #{foreman_app} -u #{foreman_user} -l #{foreman_log}"
-
-    queue %{
-      echo "-----> Exporting foreman procfile for #{foreman_app}"
-      #{echo_cmd %[cd #{deploy_to!}/#{current_path!} ; #{export_cmd}]}
-    }
-  end
-
-  desc "Start the application services"
+namespace :rackup do
+  desc "Start the application '#{application}' services"
   task :start do
-    queue %{
-      echo "-----> Starting #{foreman_app} services"
-      #{echo_cmd %[sudo start #{foreman_app}]}
-    }
+    run "cd #{current_path}; RAILS_ENV=production bundle exec rackup -E production -D -P #{pid}"
   end
 
-  desc "Stop the application services"
+  desc "Stop the application '#{application}' services"
   task :stop do
-    queue %{
-      echo "-----> Stopping #{foreman_app} services"
-      #{echo_cmd %[sudo stop #{foreman_app}]}
-    }
+    run "cd #{current_path}; if [ -f  ] && [ -e /proc/$(cat #{pid}) ]; then kill -9 `cat #{pid}`; fi"
   end
-
-  desc "Restart the application services"
+ 
+  desc "Restart the application '#{application}' services"
   task :restart do
-    queue %{
-      echo "-----> Restarting #{foreman_app} services"
-      #{echo_cmd %[sudo start #{foreman_app} || sudo restart #{foreman_app}]}
-    }
+    stop
+    start
   end
 end
 
